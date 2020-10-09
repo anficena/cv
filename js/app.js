a4a3da87d443aa44a86987e9e2718190667ca681
anficena <luthfirohmatulloh@gmail.com>
2020-10-09 21:32:08 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const educations = [
         name: "Universitas Negeri Yogyakarta",
         majors: "Pendidikan Teknik Informatika - S1",
         period: "2015 - 2019",
-        img: basePath+"/images/uny.jpg"
+        img: "https://anficena.github.io/cv/images/uny.jpg"
     },
     {
         name: "SMK Negeri 2 Wonosari",
